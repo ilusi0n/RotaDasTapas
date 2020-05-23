5eca9af2007204924e7f56b4207cf479b71e9908
Hugo Cabrita <hugo.cabrita@criticaltechworks.com>
2020-05-23 11:54:36 +0100

    minor changes

--- RotaDasTapas/Startup.cs
+++ RotaDasTapas/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using RotaDasTapas.Repository;
 using RotaDasTapas.Services;
@@ -53,11 +46,8 @@ namespace RotaDasTapas
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rota das Tapas API");
             });
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
