32f401ac7b0cf197eb5ec6c17c7d7ac5bbb4c4bc
ilusi0n <ha.cabrita@gmail.com>
2020-06-08 17:58:18 +0100

    use real data

--- RotaDasTapas/Utils/TapasUtils.cs
+++ RotaDasTapas/Utils/TapasUtils.cs
@@ -9,54 +9,78 @@ namespace RotaDasTapas.Utils
     {
         public static IEnumerable<TapaDto> InitMock()
         {
-            var tapa1 = new TapaDto
+            var LisboaTapa1 = new TapaDto
             {
-                Id = string.Join(Separators.Underscore, "Lisboa", 1),
-                Name = "ZName1",
-                Title = "Title1",
-                Address = "Address",
-                Description = "Description1",
-                City = "Lisboa",
-                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/9/18618109/fc91d7cf0ff3896fa3b6c4bd904ebd29.jpg",
-                Schedule = "07:00,15:00;1,1|07:00,14:00;6,6"
+                Id = string.Join(Separators.Underscore, "Lisbon", 1),
+                Name = "Red",
+                Title = "Tiborna de Rosbife",
+                Address = "Praça São João Bosco, nº554",
+                Description = "Rosbife, pão, batata-doce com bacon ou cogumelos",
+                City = "Lisbon",
+                ImageUrl = "https://b.zmtcdn.com/data/reviews_photos/457/3a067bf6486868b3e5f4691067e39457_1506688864.jpg",
+                Schedule = "15:00,21:00;1,5|12:00,21:30;0,0|12:00,21:30;6,6"
             };
-            var tapa2 = new TapaDto
+            var LisboaTapa2 = new TapaDto
             {
-                Id = string.Join(Separators.Underscore, "Lisboa", 2),
-                Name = "Name2",
-                Title = "Title2",
-                Address = "Address",
-                Description = "Description2",
-                City = "Lisboa",
-                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/9/18618109/fc91d7cf0ff3896fa3b6c4bd904ebd29.jpg",
-                Schedule = "07:00,15:00;1,1|07:00,14:00;6,6"
-            };
-
-            var tapa3 = new TapaDto
-            {
-                Id = string.Join(Separators.Underscore, "Lisboa", 3),
-                Name = "Name3",
-                Title = "Title3",
-                Address = "Address",
-                Description = "Description3",
-                City = "Lisboa",
-                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/9/18618109/fc91d7cf0ff3896fa3b6c4bd904ebd29.jpg",
-                Schedule = "07:00,15:00;1,1|07:00,14:00;6,6"
+                Id = string.Join(Separators.Underscore, "Lisbon", 2),
+                Name = "Verde Gaio",
+                Title = "Tapa do Chef",
+                Address = "Rua Francisco Metrass, nº18, B",
+                Description = "Farinheira assada na brasa com alface francesa",
+                City = "Lisbon",
+                ImageUrl = "https://b.zmtcdn.com/data/reviews_photos/3d8/f9db351dec4e65052a00f9e0364d13d8_1491355337.jpg",
+                Schedule = "13:00,23:00;1,6"
             };
 
-            var tapa4 = new TapaDto
+            var LisboaTapa3 = new TapaDto
             {
-                Id = string.Join(Separators.Underscore, "Lisboa", 4),
-                Name = "Name1",
-                Title = "Title1",
-                Address = "Address",
-                Description = "Description1",
-                City = "Lisboa",
-                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/9/18618109/fc91d7cf0ff3896fa3b6c4bd904ebd29.jpg",
-                Schedule = "07:00,15:00;1,1|07:00,14:00;6,6"
+                Id = string.Join(Separators.Underscore, "Lisbon", 3),
+                Name = "Choco do Bairro",
+                Title = "Choco à Pescador",
+                Address = "Rua Tenente Ferreira Durão, Nº 55, A",
+                Description = "Guisado de Choco, acompanhado com pão dourado e coentros",
+                City = "Lisbon",
+                ImageUrl = "https://b.zmtcdn.com/data/reviews_photos/b30/49a3e35fe3e566cd8b1fcda57bf94b30_1545936190.jpg",
+                Schedule = "18:00,21:30;1,6"
             };
 
-            var tapa5 = new TapaDto
+            var LisboaTapa4 = new TapaDto
+            {
+                Id = string.Join(Separators.Underscore, "Lisbon", 4),
+                Name = "Pigmeu",
+                Title = "Oinki Satay",
+                Address = "Rua 4 de Infantaria, nº68",
+                Description = "Espetadinha de cachaço marinada em Estrella Damm e grelhada com molho de amendoim",
+                City = "Lisbon",
+                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/1/8211481/3ed9649189f68527a20c9c8aba75ea51.jpg",
+                Schedule = "12:30,14:30;2,6|12:30,14:30;0,0|18:45,22:30;2,6|18:45,22:30;0,0"
+            };
+            
+            var LisboaTapa5 = new TapaDto
+            {
+                Id = string.Join(Separators.Underscore, "Lisbon", 5),
+                Name = "Moulles & Beer",
+                Title = "Mexilhão Panado",
+                Address = "Rua 4 de Infantaria, Nº29, B",
+                Description = "Mexilhão panado com molho de sweet, chilli e maionese",
+                City = "Lisbon",
+                ImageUrl = "4https://b.zmtcdn.com/data/pictures/4/8201164/44384b8b6643e6c988f71c1fc7071544.jpg",
+                Schedule = "19:00,23:00;0,6"
+            };
+            
+            var LisboaTapa6 = new TapaDto
+            {
+                Id = string.Join(Separators.Underscore, "Lisbon", 6),
+                Name = "Pequeno Reino",
+                Title = "Quibes Vegetarianos de Lentilhas",
+                Address = "Rua 4 de Infantaria, Nº32, A",
+                Description = "Porção de quatro quibes de lentihas fritos",
+                City = "Lisbon",
+                ImageUrl = "https://b.zmtcdn.com/data/pictures/chains/4/18619594/1853a0bdcac91572dc9ae86187527099.jpg",
+                Schedule = "15:00,21:00;2,6|15:00,21:00;0,0"
+            };
+
+            var PortoTapa1 = new TapaDto
             {
                 Id = string.Join(Separators.Underscore, "Porto", 1),
                 Name = "Name1",
@@ -68,48 +92,50 @@ namespace RotaDasTapas.Utils
                 Schedule = "07:00,15:00;1,1|07:00,14:00;6,6"
             };
 
-            tapa1.Path = new List<Path>
+            LisboaTapa1.Path = new List<Path>
             {
-                CreatePath(tapa1.Id, 0),
-                CreatePath(tapa2.Id, 5),
-                CreatePath(tapa3.Id, 1),
-                CreatePath(tapa4.Id, 500)
+                CreatePath(LisboaTapa1.Id, 0),
+                CreatePath(LisboaTapa2.Id, 5),
+                CreatePath(LisboaTapa3.Id, 1),
+                CreatePath(LisboaTapa4.Id, 500)
             };
-            tapa2.Path = new List<Path>
+            LisboaTapa2.Path = new List<Path>
             {
-                CreatePath(tapa1.Id, tapa1.Path.ToList()[1].Distance),
-                CreatePath(tapa2.Id, 0),
-                CreatePath(tapa3.Id, 40),
-                CreatePath(tapa4.Id, 50)
+                CreatePath(LisboaTapa1.Id, LisboaTapa1.Path.ToList()[1].Distance),
+                CreatePath(LisboaTapa2.Id, 0),
+                CreatePath(LisboaTapa3.Id, 40),
+                CreatePath(LisboaTapa4.Id, 50)
             };
-            tapa3.Path = new List<Path>
+            LisboaTapa3.Path = new List<Path>
             {
-                CreatePath(tapa1.Id, tapa1.Path.ToList()[2].Distance),
-                CreatePath(tapa2.Id, tapa2.Path.ToList()[2].Distance),
-                CreatePath(tapa3.Id, 0),
-                CreatePath(tapa4.Id, 500)
+                CreatePath(LisboaTapa1.Id, LisboaTapa1.Path.ToList()[2].Distance),
+                CreatePath(LisboaTapa2.Id, LisboaTapa2.Path.ToList()[2].Distance),
+                CreatePath(LisboaTapa3.Id, 0),
+                CreatePath(LisboaTapa4.Id, 500)
             };
-            tapa4.Path = new List<Path>
+            LisboaTapa4.Path = new List<Path>
             {
-                CreatePath(tapa1.Id, tapa1.Path.ToList()[3].Distance),
-                CreatePath(tapa2.Id, tapa2.Path.ToList()[3].Distance),
-                CreatePath(tapa3.Id, tapa3.Path.ToList()[3].Distance),
-                CreatePath(tapa4.Id, 0)
+                CreatePath(LisboaTapa1.Id, LisboaTapa1.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa2.Id, LisboaTapa2.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa3.Id, LisboaTapa3.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa4.Id, 0)
             };
-            tapa5.Path = new List<Path>
+            LisboaTapa5.Path = new List<Path>
             {
-                CreatePath(tapa1.Id, tapa1.Path.ToList()[3].Distance),
-                CreatePath(tapa2.Id, tapa2.Path.ToList()[3].Distance),
-                CreatePath(tapa3.Id, tapa3.Path.ToList()[3].Distance),
-                CreatePath(tapa4.Id, 0)
+                CreatePath(LisboaTapa1.Id, LisboaTapa1.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa2.Id, LisboaTapa2.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa3.Id, LisboaTapa3.Path.ToList()[3].Distance),
+                CreatePath(LisboaTapa4.Id, 0)
             };
 
             return new List<TapaDto>
             {
-                tapa1,
-                tapa2,
-                tapa3,
-                tapa4
+                LisboaTapa1,
+                LisboaTapa2,
+                LisboaTapa3,
+                LisboaTapa4,
+                LisboaTapa5,
+                LisboaTapa6
             };
         }
 
