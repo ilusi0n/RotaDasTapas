05238e8c46538bc6097c9f31ebfeb71ded8ce2e4
ilusi0n <ha.cabrita@gmail.com>
2020-06-11 02:15:45 +0100

    fix DI

--- RotaDasTapas/Models/Request/TapasParameters.cs
+++ RotaDasTapas/Models/Request/TapasParameters.cs
@@ -4,6 +4,9 @@ namespace RotaDasTapas.Models.Request
 {
     public class TapasParameters
     {
+        /// <summary>
+        /// Current datetime of the client used for schedule algorithm
+        /// </summary>
         [Required] public string Localtime { get; set; }
     }
 }
--- RotaDasTapas/Startup.cs
+++ RotaDasTapas/Startup.cs
@@ -41,6 +41,7 @@ namespace RotaDasTapas
             services.AddScoped<ITapasGateway, TapasGateway>();
             services.AddScoped<IJourneyUtils, JourneyUtils>();
             services.AddScoped<ITravellingSalesmanProblem, TravellingSalesmanProblem>();
+            services.AddScoped<IBusinessUtils, BusinessHoursUtils>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
