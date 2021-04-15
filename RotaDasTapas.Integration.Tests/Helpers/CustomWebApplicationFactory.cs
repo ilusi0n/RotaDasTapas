aca50be8fa5daf33d81fadfe6405caba796c89ae
ilusi0n <ha.cabrita@gmail.com>
2021-04-15 14:48:07 +0100

    .

--- RotaDasTapas.Integration.Tests/Helpers/CustomWebApplicationFactory.cs
+++ RotaDasTapas.Integration.Tests/Helpers/CustomWebApplicationFactory.cs
@@ -9,8 +9,6 @@ namespace RotaDasTapas.Integration.Tests.Helpers
     {
         protected override void ConfigureClient(HttpClient client)
         {
-            client.DefaultRequestHeaders.Add("BMW-Correlation-ID", "10000000-0000-0000-0000-000000000002");
-            client.DefaultRequestHeaders.Add("BMW-SCS-Search-Configuration", "crypto-string");
             client.BaseAddress = new Uri("http://localhost:5001");
             base.ConfigureClient(client);
         }
